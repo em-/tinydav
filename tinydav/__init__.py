a4fe32c2a3cf62ccdfbc88f1597b0423dd87bea1
Manuel Hermann <manuel-hermann@gmx.net>
2010-12-11 11:18:39 +0100

    Raise version.

--- tinydav/__init__.py
+++ tinydav/__init__.py
@@ -34,7 +34,7 @@ from tinydav.exception import HTTPError, HTTPUserError, HTTPServerError
 
 __author__ = "Manuel Hermann <manuel-hermann@gmx.net>"
 __license__ = "LGPL"
-__version__ = "0.6.3"
+__version__ = "0.6.4"
 
 __all__ = (
     "HTTPError", "HTTPUserError", "HTTPServerError",
